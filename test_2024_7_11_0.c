ec034e4426e5248f805bdef5a841dd28b1436fc2
Raykevin-live <raykevin121@gmail.com>
2024-07-11 09:58:59 +0800

    用宏实现offsetof

---
+++ test_2024_7_11_0.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//用宏实现offsetof
+typedef struct S
+{
+	char c1;
+	int a;
+	char c2;
+}S;
+
+#define OFFSETOF(struct_name, member_name) (int)&(((struct_name*)0)->member_name)//赋给0地址，更容易显示偏移量
+
+int main()
+{
+	printf("%d\n", OFFSETOF(S, c1));
+	printf("%d\n", OFFSETOF(S, a));
+	printf("%d\n", OFFSETOF(S, c2));
+	
+	return 0;
+}
