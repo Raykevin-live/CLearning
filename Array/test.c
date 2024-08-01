bd05c1fdd3d017ee92903ca8c2f102c87f24e09f
Raykevin-live <raykevin121@gmail.com>
2024-08-02 00:12:43 +0800

    二维数组的应用

--- Array/test.c
+++ Array/test.c
@@ -103,50 +103,50 @@
 //有一个数字矩阵，矩阵的每行从左到右是递增的，矩阵从上到下是递增的，
 //请编写程序在这样的矩阵中查找某个数字是否存在。
 
-int FindNum(int arr[3][3], int k, int x, int y)
-{
-	int row = 0;
-	int col = y - 1;
-	while (row <= x - 1 && col >= 0)
-	{
-		if (arr[row][col] > k)
-		{
-			col--;
-		}
-		else if (arr[row][col] < k)
-		{
-			row++;
-		}
-		else
-		{
-			return 1;
-		}
-	}
-	return 0;
-}
-
-int main()
-{
-	int arr[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9 } };
-	int k = 1;
-	int x = 3;
-	int y = 3;
-
-	//返回型参数
-	int ret = FindNum(arr, k, x, y);
-
-	if (1 == ret)
-	{
-		printf("找到了\n");
-	}
-	else
-	{
-		printf("找不到\n");
-	}
-
-
-	return 0;
-}
+//int FindNum(int arr[3][3], int k, int x, int y)
+//{
+//	int row = 0;
+//	int col = y - 1;
+//	while (row <= x - 1 && col >= 0)
+//	{
+//		if (arr[row][col] > k)
+//		{
+//			col--;
+//		}
+//		else if (arr[row][col] < k)
+//		{
+//			row++;
+//		}
+//		else
+//		{
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9 } };
+//	int k = 1;
+//	int x = 3;
+//	int y = 3;
+//
+//	//返回型参数
+//	int ret = FindNum(arr, k, x, y);
+//
+//	if (1 == ret)
+//	{
+//		printf("找到了\n");
+//	}
+//	else
+//	{
+//		printf("找不到\n");
+//	}
+//
+//
+//	return 0;
+//}
 
 
 
@@ -157,3 +157,67 @@ int main()
 //D说：C在胡说
 //已知3个人说了真话，1个人说的是假话。
 //现在请根据这些信息，写一个程序来确定到底谁是凶手。
+
+//int main()
+//{
+//	char thief = '0';
+//	char ch[4] = "ABCD";
+//	for (int i = 0; i < 4; i++)
+//	{
+//		thief = ch[i];
+//		if (thief == 'A' && thief == 'C' && thief == 'D' && thief != 'D')
+//		{
+//			break;
+//		}
+//		else if (thief != 'A' && thief != 'C' && thief == 'D' && thief != 'D')
+//		{
+//			break;
+//		}
+//		else if (thief != 'A' && thief == 'C' && thief != 'D')
+//		{
+//			break;
+//		}
+//		else if (thief != 'A' && thief == 'C' && thief == 'D')
+//		{
+//			break;
+//		}
+//	}
+//	printf("The thief is %c\n", thief);
+//	
+//	return 0;
+//}
+
+
+//在屏幕上打印杨辉三角
+
+int main()
+{
+	int n = 0;
+	int tri[50][50] = {0};
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j || j == 0)
+			{
+				tri[i][j] = 1;
+			}
+			else if (i > j)
+			{
+				tri[i][j] = tri[i - 1][j - 1] + tri[i - 1][j];
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("%d ", tri[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
